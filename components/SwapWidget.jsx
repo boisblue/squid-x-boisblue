a1715ea6fd4dbd6bda1e330d209a3c097cd25c98
boisblue <149630680+boisblue@users.noreply.github.com>
2026-05-25 16:55:15 +0000

    chore: random config #1/10

--- components/SwapWidget.jsx
+++ components/SwapWidget.jsx
@@ -19,7 +19,7 @@ export default function SwapWidget() {
           "button-sm-secondary": "2.25rem",
           "button-sm-tertiary": "1.25rem",
           "container": "1.25rem",
-          "input": "9999px",
+          "input": "1.75rem",
           "menu-sm": "0.75rem",
           "menu-lg": "1.15rem",
           "modal": "3.15rem"
