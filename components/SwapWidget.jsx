d2ecc395cfeafe2db521b85f9241b625378e2d43
boisblue <149630680+boisblue@users.noreply.github.com>
2026-06-01 19:49:59 +0000

    chore: random config #10/10

--- components/SwapWidget.jsx
+++ components/SwapWidget.jsx
@@ -28,7 +28,7 @@ export default function SwapWidget() {
           "caption": "0.95rem",
           "body-small": "1.25rem",
           "body-medium": "1.35rem",
-          "body-large": "1.75rem",
+          "body-large": "1.95rem",
           "heading-small": "2.45rem",
           "heading-medium": 600,
           "heading-large": "4.15rem"
@@ -37,7 +37,7 @@ export default function SwapWidget() {
           "caption": "0.95rem",
           "body-small": "1.25rem",
           "body-medium": "1.35rem",
-          "body-large": "1.75rem",
+          "body-large": "1.95rem",
           "heading-small": "2.45rem",
           "heading-medium": 600,
           "heading-large": "4.15rem"
