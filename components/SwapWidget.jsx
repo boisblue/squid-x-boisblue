f25fa11fc4302de8d36e250204b869bb9a4e3519
boisblue <149630680+boisblue@users.noreply.github.com>
2026-06-07 16:05:23 +0000

    chore: random config #7/10

--- components/SwapWidget.jsx
+++ components/SwapWidget.jsx
@@ -104,7 +104,7 @@ export default function SwapWidget() {
       },
       "priceImpactWarnings": {
         "warning": 4,
-        "critical": 7
+        "critical": 6
       },
       "loadPreviousStateFromLocalStorage": true
     }} />
