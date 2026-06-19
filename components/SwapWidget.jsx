ccdec91746e36571b8903145d11639efe9f37dc8
boisblue <149630680+boisblue@users.noreply.github.com>
2026-06-19 17:08:22 +0000

    chore: random config #9/10

--- components/SwapWidget.jsx
+++ components/SwapWidget.jsx
@@ -59,7 +59,7 @@ export default function SwapWidget() {
           "grey-800": "#292C32",
           "grey-900": "#17191C",
           "royal-300": "#D9BEF4",
-          "royal-400": "#B893EC",
+          "royal-400": "#C8A8FF",
           "royal-500": "#A67BFF",
           "royal-600": "#6F3EBF",
           "royal-700": "#6B45A1",
