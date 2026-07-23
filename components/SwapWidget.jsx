be5ecef27d71048a67edbc86e5578328a8c110d3
boisblue <149630680+boisblue@users.noreply.github.com>
2026-07-23 16:17:11 +0000

    chore: random config #3/10

--- components/SwapWidget.jsx
+++ components/SwapWidget.jsx
@@ -11,7 +11,7 @@ export default function SwapWidget() {
         "borderRadius": {
           "button-lg-primary": "2.25rem",
           "button-lg-secondary": "3.45rem",
-          "button-lg-tertiary": "2.95rem",
+          "button-lg-tertiary": "4.15rem",
           "button-md-primary": "2.65rem",
           "button-md-secondary": "1.95rem",
           "button-md-tertiary": "1.25rem",
