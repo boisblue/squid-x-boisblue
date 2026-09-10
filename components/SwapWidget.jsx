a3fc7f003af911c6d7c7fd29cd1b1350fcb7304a
boisblue <149630680+boisblue@users.noreply.github.com>
2026-09-10 17:56:22 +0000

    chore: random config #9/10

--- components/SwapWidget.jsx
+++ components/SwapWidget.jsx
@@ -66,7 +66,7 @@ export default function SwapWidget() {
           "status-positive": "#7AE870",
           "status-negative": "#FF4D5B",
           "status-partial": "#F3AF25",
-          "highlight-700": "#E4FE53",
+          "highlight-700": "#C5FF2A",
           "animation-bg": "#9E79D2",
           "animation-text": "#FBFBFD",
           "button-lg-primary-bg": "#7B4ED9",
